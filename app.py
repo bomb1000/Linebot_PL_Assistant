420a2b8282a94a5820553d84a02bc14fbe0627e9
bomb1000 <michael491549154915@gmail.com>
2024-05-11 16:16:06 +0800

    LLM to linebot test

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, request, abort
+# configuration = Configuration(access_token='vbe05kdIZq9OHDPEYphwEiWWeceVm+9ONTcxVC1bkchDji4oWPfQxL8jdEv4sLo72lMz0udPy42qi2ZjA1asKBNrwelxDCI1ih1sK9gjDFLooEHRpn+fyA5yOZ/YUozTCnHPgoippjkrMs3LrfcPFwdB04t89/1O/w1cDnyilFU=')
+# handler = WebhookHandler('6caabad552d4be95d53916288baa3d1e')
 
+from flask import Flask, request, abort
 from linebot.v3 import (
     WebhookHandler
 )
@@ -17,12 +19,14 @@ from linebot.v3.webhooks import (
     MessageEvent,
     TextMessageContent
 )
+from transformers import pipeline
 
 app = Flask(__name__)
 
-configuration = Configuration(access_token='vbe05kdIZq9OHDPEYphwEiWWeceVm+9ONTcxVC1bkchDji4oWPfQxL8jdEv4sLo72lMz0udPy42qi2ZjA1asKBNrwelxDCI1ih1sK9gjDFLooEHRpn+fyA5yOZ/YUozTCnHPgoippjkrMs3LrfcPFwdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('6caabad552d4be95d53916288baa3d1e')
-
+# Set your LINE access token and Hugging Face model here
+configuration = Configuration(access_token='YOUR_LINE_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_LINE_SECRET')
+chat_pipeline = pipeline("text-generation", model="MediaTek-Research/Breeze-7B-Instruct-v1_0")
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -42,15 +46,17 @@ def callback():
 
     return 'OK'
 
-
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
+    user_message = event.message.text
+    response_text = chat_pipeline(user_message, max_length=50)[0]['generated_text']
+
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
         line_bot_api.reply_message_with_http_info(
             ReplyMessageRequest(
                 reply_token=event.reply_token,
-                messages=[TextMessage(text=event.message.text)]
+                messages=[TextMessage(text=response_text)]
             )
         )
 
